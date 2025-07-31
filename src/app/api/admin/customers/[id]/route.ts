ca56a40a8ec9fb781ebd8eb5ff7e8b19cdef505e
iamgodwinide <iamgodwinide@gmail.com>
2025-07-31 23:02:14 +0100

    new

--- src/app/api/admin/customers/[id]/route.ts
+++ src/app/api/admin/customers/[id]/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth/next';
-import { authOptions } from '../../../auth/[...nextauth]/route';
+import { authOptions } from '../../../auth/[...nextauth]/config';
 import { connectDb } from '@/config';
 import Customer from '@/models/Customer';
 import bcrypt from 'bcryptjs';
